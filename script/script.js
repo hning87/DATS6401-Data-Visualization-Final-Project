2c1207b37c9088e2fe76ea36c81e4bc732c2b554
hning87 <49322546+hning87@users.noreply.github.com>
2020-05-03 11:26:55 -0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// side_navigator
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+
